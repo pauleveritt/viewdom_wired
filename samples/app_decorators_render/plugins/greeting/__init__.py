a2e483cdb903d0079c29a39a68678f9a58ca872c
Paul Everitt <pauleveritt@me.com>
2020-03-31 19:36:17 -0400

    Clean up imports.

--- samples/app_decorators_render/plugins/greeting/__init__.py
+++ samples/app_decorators_render/plugins/greeting/__init__.py
@@ -4,11 +4,16 @@ A plugin that can be installed with pip and then register its components.
 
 """
 
+from . import greeting
 from .greeting import Greeting
 from ...app import App
-from ...plugins import greeting
 
 
 def setup(app: App):
-    # Scan for decorators
     app.scanner.scan(greeting)
+
+
+__all__ = [
+    'Greeting',
+    'setup'
+]
