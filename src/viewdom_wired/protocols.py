8b58f70bd88bcfbe33850000ad312d1df1a8aa97
Paul Everitt <pauleveritt@me.com>
2021-01-01 14:53:05 -0500

    Make sure Python 3.9 works without installing typing extensions.

--- src/viewdom_wired/protocols.py
+++ src/viewdom_wired/protocols.py
@@ -1,5 +1,9 @@
-from typing_extensions import Protocol
 from viewdom import VDOM
+
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extensions import Protocol  # type: ignore
 
 
 class Component(Protocol):
